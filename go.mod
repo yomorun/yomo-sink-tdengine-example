a4c4ccf0369520ba0ba2b02ff517dc53df8b73fb
fanweixiao <fanweixiao@gmail.com>
2021-02-01 03:23:35 -0800

    integrate yomo to TDengine

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/yomorun/yomo-sink-tdengine-example
 
 go 1.13
 
-require github.com/taosdata/driver-go v0.0.0-20210127143907-31dc97ac06b1
+require (
+	github.com/taosdata/driver-go v0.0.0-20210127143907-31dc97ac06b1
+	github.com/yomorun/yomo v0.7.5 // indirect
+)
--- main.go
+++ main.go
@@ -3,13 +3,61 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/taosdata/driver-go/taosSql"
 	"os"
+	"log"
+    "context"
+
+	y3 "github.com/yomorun/y3-codec-golang"
+	"github.com/yomorun/yomo/pkg/quic"
+	_ "github.com/taosdata/driver-go/taosSql"
 )
 
 var url = "root:taosdata@/tcp(172.16.0.191:6030)/yomo"
 
 func main() {
+	log.Print("Starting YoMo Sink server: -> TDEngine")
+	srv := quic.NewServer(&srvHandler{})
+	err := srv.ListenAndServe(context.Background(), "0.0.0.0:9333")
+	if err != nil {
+		log.Printf("YoMo Sink server start failed: %s\n", err.Error())
+	}
+	select {}
+}
+
+type srvHandler struct {}
+
+func (s *srvHandler) Listen() error {
+	return nil
+}
+
+func (s *srvHandler) Read(qs quic.Stream) error {
+	ch := y3.FromStream(qs).
+		Subscribe(0x10).
+		OnObserve(decode)
+
+	go func() {
+		for item := range ch {
+			err := store(item)
+			if err != nil {
+				log.Printf("write to TDEngine error : %s", err.Error())
+			} else {
+				log.Printf("saved: %v", item)
+			}
+		}
+	}()
+
+	return nil
+}
+
+func decode(v []byte) (interface{}, error) {
+	data, err := y3.ToFloat32(v)
+	if err != nil {
+		log.Printf("err: %s\n", err.Error())
+	}
+	return data, err
+}
+
+func store(v interface{}) error {
 	db, err := sql.Open("taosSql", url)
 	if err != nil {
 		fmt.Printf("Open database error: %s\n", err)
@@ -26,7 +74,7 @@ func main() {
 	fmt.Printf("res=%v\n", res)
 
 	// Insert data
-	var val float32 = 5.83
+	var val float32 = v.(float32)
 	sql = "INSERT INTO noise VALUES (NOW, " + fmt.Sprintf("%f", val) + ")"
 	res, err = db.Exec(sql)
 	if err != nil {
@@ -34,4 +82,5 @@ func main() {
 	}
 	fmt.Printf("res=%v\n", res)
 
+	return err
 }
